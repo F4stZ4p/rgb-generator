fce5095e1177a775f01052793d37ccaff49a8aa9
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-04-04 21:43:28 +0300

    Add files via upload

---
+++ RGBgen.bat
@@ -0,0 +1,29 @@
+:event_do.this.firstly
+@echo off
+color F0
+title RGB Generator by F4stZ4p
+mode con:cols=56 lines=5
+
+:event_main
+echo -------------------------------------------------------
+echo # RGB Generator by F4stZ4p. Press any key to generate #
+echo -------------------------------------------------------
+pause >nul
+
+:event_color.gen
+cls
+set MIN=0
+set MAX=255
+set /a R=MIN+(MAX-MIN+1) * %random%/32768
+set MIN=0
+set MAX=255
+set /a G=MIN+(MAX-MIN+1) * %random%/32768
+set MIN=0
+set MAX=255
+set /a B=MIN+(MAX-MIN+1) * %random%/32768
+echo Generated: %R%,%G%,%B%
+@echo %R%,%G%,%B%| Clip
+echo RGB copied to clipboard!
+echo Press any key to generate again.
+pause >nul
+goto :event_color.gen
